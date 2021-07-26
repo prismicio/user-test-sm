2074e33157628d5710998da06d79d0b19914bb49
Benjamin <benjammartin@gmail.com>
2021-07-26 08:49:13 +0200

    add slice

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="py-10">
-    <div class="pb-5">
+    <div class="pb-8">
       <h1 class="text-4xl font-bold mb-4">
         Start here for a quick overview of everything you need to know
       </h1>
-      <p>
+      <p class="mb-4">
         This your page builder. Inside this area, you'll find straightforward
         controls for adding slices, modify your content and format it. For now,
         you're probably just wondering what to do first. To help get you going
@@ -12,6 +12,9 @@
         (like this doc!) covering all the key concepts and features of the
         product.
       </p>
+      <a class="text-blue-500 font-bold" href="https://prismic.io"
+        >Learn more</a
+      >
     </div>
     <slice-zone type="getting-started" queryType="single" />
   </section>
--- slices/Summary/index.vue
+++ slices/Summary/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <section class="">
+  <section class="border-t-2 border-b-2 border-dashed py-8">
     <prismic-rich-text
       :field="slice.primary.title"
-      class="text-2xl font-bold"
+      class="text-2xl font-bold mb-3"
     />
-    <ul id="example-1">
-      <li v-for="item in slice.items" :key="item.message">
+    <ul id="example-1" class="list-disc">
+      <li
+        class="ml-5 text-green-50"
+        v-for="item in slice.items"
+        :key="item.message"
+      >
         <prismic-rich-text :field="item.item" class="text-1xl font-bold" />
       </li>
     </ul>
