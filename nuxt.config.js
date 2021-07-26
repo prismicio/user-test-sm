e67adfdfc8c047a6bac87e544677c3de36b00999
Benjamin <benjammartin@gmail.com>
2021-07-26 10:12:11 +0200

    remove split

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,48 +1,49 @@
-import { getStoriesPaths } from 'slice-machine-ui/helpers/storybook';
+import { getStoriesPaths } from "slice-machine-ui/helpers/storybook";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'User test - project-name',
+    title: "User test - project-name",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
-    meta: [{
-      charset: 'utf-8'
-    }, {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1'
-    }, {
-      hid: 'description',
-      name: 'description',
-      content: ''
-    }],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }],
+    meta: [
+      {
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: ""
+      }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ],
     script: []
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@fontsource/montserrat", "@fontsource/yeseva-one", "splitting/dist/splitting.css", "splitting/dist/splitting-cells.css"],
+  css: ["@fontsource/montserrat", "@fontsource/yeseva-one"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/splitting.client.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    "@nuxtjs/prismic",
-    '@nuxtjs/tailwindcss',
-    "@nuxt/image"
-  ],
+  buildModules: ["@nuxtjs/prismic", "@nuxtjs/tailwindcss", "@nuxt/image"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["nuxt-sm"],
